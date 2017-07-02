5769726384fc64be2316f26897aaa6a2f8ae359e
Will Lilley <will.lilley93@gmail.com>
2017-07-02 21:24:18 +0100

    Version 1.0.9
    Major Changes:
    Minor Changes: Completed Array

--- js/teamarray.js
+++ js/teamarray.js
@@ -2,9 +2,27 @@
 
 var teamarray = [
   {"name": "Arsenal", "code": "ARS", "key": "arsenal"},
-  {"name": "Bournemouth", "code": "BOR", "key": "bournemouth"},
+  {"name": "Bournemouth", "code": "BOU", "key": "bournemouth"},
   {"name": "Burnley", "code": "BUR", "key": "burnley"},
   {"name": "Chelsea", "code": "CHE", "key": "chelsea"},
+  {"name": "Crystal Palace", "code": "CRY", "key": "crystalpalace"},
+  {"name": "Everton", "code": "EVE", "key": "everton"},
+  {"name": "Hull City", "code": "HUL", "key": "hull"},
+  {"name": "Leicester City", "code": "LEI", "key": "leicester"},
+  {"name": "Liverpool", "code": "LIV", "key": "liverpool"},
+  {"name": "Manchester City", "code": "MCI", "key": "mancity"},
+  {"name": "Manchester United", "code": "MUN", "key": "manutd"},
+  {"name": "Middlesbrough", "code": "MFC", "key": "middlesbrough"},
+  {"name": "Southampton", "code": "SOU", "key": "southampton"},
+  {"name": "Stoke City", "code": "STK", "key": "stoke"},
+  {"name": "Sunderland", "code": "SUN", "key": "sunderland"},
+  {"name": "Swansea", "code": "SWA", "key": "swansea"},
+  {"name": "Tottenham Hotspur", "code": "TOT", "key": "tottenham"},
+  {"name": "Watford", "code": "WAT", "key": "watford"},
+  {"name": "West Bromwich Albion", "code": "WBA", "key": "westbrom"},
+  {"name": "West Ham United", "code": "WHU", "key": "westham"},
+
+
 ]
 
 function testArray() {
